6ba353fd0f6576f2110ae55953e0fe2ecce8fb5f
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-15 09:38:42 +0700

    Create KoprasiDemo.java

--- P3/src/operasigettersetter/KoprasiDemo.java
+++ P3/src/operasigettersetter/KoprasiDemo.java
@@ -1,2 +1,16 @@
-package operasigettersetter;public class KoprasiDemo {
+package operasigettersetter;
+
+public class KoprasiDemo {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota("Iwan", "Jalan Mawar");
+        System.out.println("Anggota " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+
+        anggota1.setNama("Iwan Setiawan");
+        anggota1.setAlamat("Jalan Soekarno Hata no 18");
+        anggota1.setor(100000);
+        System.out.println("Anggota " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+
+        anggota1.pinjam(50000);
+        System.out.println("Anggota " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+    }
 }

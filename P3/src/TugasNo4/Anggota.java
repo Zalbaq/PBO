4db7aa2ac3014987428b681541a35d8c76178df8
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-15 09:41:17 +0700

    Create Anggota.java

--- P3/src/TugasNo4/Anggota.java
+++ P3/src/TugasNo4/Anggota.java
@@ -1,2 +1,48 @@
-package TugasNo4;public class Anggota {
+package TugasNo4;
+
+public class Anggota {
+    private String nomorKTP;
+    private String nama;
+    private double limitPinjaman;
+    private double jumlahPinjaman;
+
+    public Anggota(String nomorKTP, String nama, double limitPinjaman) {
+        this.nomorKTP = nomorKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0; // Awalnya jumlah pinjaman adalah 0
+    }
+
+    public String getNomorKTP() {
+        return nomorKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public double getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(double jumlah) {
+        if (jumlah <= limitPinjaman - jumlahPinjaman) {
+            jumlahPinjaman += jumlah;
+        } else {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        }
+    }
+
+    public void angsur(double jumlah) {
+        if (jumlah <= jumlahPinjaman) {
+            jumlahPinjaman -= jumlah;
+        } else {
+            System.out.println("Maaf, jumlah angsuran melebihi jumlah pinjaman.");
+        }
+    }
 }
+

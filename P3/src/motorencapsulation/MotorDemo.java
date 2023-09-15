f3cbd08edf772970aeb1ee192c0929c72f1965f3
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-15 09:37:21 +0700

    Create MotorDemo.java

--- P3/src/motorencapsulation/MotorDemo.java
+++ P3/src/motorencapsulation/MotorDemo.java
@@ -1,2 +1,24 @@
-package motorencapsulation;public class MotorDemo {
+package motorencapsulation;
+
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor = new Motor();
+        motor.printStatud();
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatud();
+
+        motor.tambahKecepatan();
+        motor.printStatud();
+
+        motor.tambahKecepatan();
+        motor.printStatud();
+
+        motor.tambahKecepatan();
+        motor.printStatud();
+
+        motor.matikanMesin();
+        motor.printStatud();
+    }
 }

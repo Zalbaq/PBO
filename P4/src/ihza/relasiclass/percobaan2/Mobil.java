b41feba91f6e2b51c9e3f80723c5ac50471b83b8
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-22 09:51:26 +0700

    create Mobil class

--- P4/src/ihza/relasiclass/percobaan2/Mobil.java
+++ P4/src/ihza/relasiclass/percobaan2/Mobil.java
@@ -1,2 +1,28 @@
-package ihza.relasiclass.percobaan2;public class Mobil {
+package ihza.relasiclass.percobaan2;
+
+public class Mobil {
+    private String merk;
+    private int biaya;
+
+    public Mobil() {}
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+
+    public int hitungBiayaMobil(int hari) {
+        return biaya * hari;
+    }
 }

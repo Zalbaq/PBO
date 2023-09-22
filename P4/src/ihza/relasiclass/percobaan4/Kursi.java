6c730fda08252524aa31b859a5f28bd8720ed64b
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-22 09:54:15 +0700

    create Kursi class

--- P4/src/ihza/relasiclass/percobaan4/Kursi.java
+++ P4/src/ihza/relasiclass/percobaan4/Kursi.java
@@ -1,2 +1,34 @@
-package ihza.relasiclass.percobaan4;public class Kursi {
+package ihza.relasiclass.percobaan4;
+
+public class Kursi {
+    private String nomor;
+    private Penumpang penumpang;
+
+    public Kursi(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public Penumpang getPenumpang() {
+        return penumpang;
+    }
+
+    public void setPenumpang(Penumpang penumpang) {
+        this.penumpang = penumpang;
+    }
+    public String info() {
+        String info = "";
+        info += "Nomor: " + nomor + "\n";
+        if (this.penumpang != null) {
+            info += "Penumpang: " + penumpang.info() + "\n";
+        }
+        return info;
+    }
 }

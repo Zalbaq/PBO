880b227dc64035c4f3a5c9135472517687c3dd37
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-22 09:51:46 +0700

    create Pelanggan class

--- P4/src/ihza/relasiclass/percobaan2/Pelanggan.java
+++ P4/src/ihza/relasiclass/percobaan2/Pelanggan.java
@@ -1,2 +1,46 @@
-package ihza.relasiclass.percobaan2;public class Pelanggan {
+package ihza.relasiclass.percobaan2;
+
+public class Pelanggan {
+    public Pelanggan() {}
+    private String nama;
+    private Mobil mobil;
+    private Sopir sopir;
+    private int hari;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Mobil getMobil() {
+        return mobil;
+    }
+
+    public void setMobil(Mobil mobil) {
+        this.mobil = mobil;
+    }
+
+    public Sopir getSopir() {
+        return sopir;
+    }
+
+    public void setSopir(Sopir sopir) {
+        this.sopir = sopir;
+    }
+
+    public int getHari() {
+        return hari;
+    }
+
+    public void setHari(int hari) {
+        this.hari = hari;
+    }
+
+    public int hitungBiayaTotal() {
+        return mobil.hitungBiayaMobil(hari) +
+                sopir.hitungBiayaSopir(hari);
+    }
 }

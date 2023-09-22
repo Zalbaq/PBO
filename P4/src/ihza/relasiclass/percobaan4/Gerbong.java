ecd061a6966bbcf69bc31ced8944223dd176baf8
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-22 09:54:00 +0700

    create Gerbong class

--- P4/src/ihza/relasiclass/percobaan4/Gerbong.java
+++ P4/src/ihza/relasiclass/percobaan4/Gerbong.java
@@ -1,2 +1,28 @@
-package ihza.relasiclass.percobaan4;public class Gerbong {
+package ihza.relasiclass.percobaan4;
+
+public class Gerbong {
+    private String kode;
+    private Kursi[] arrayKursi;
+
+    public Gerbong(String kode,  int jumlah) {
+        this.kode = kode;
+        this.arrayKursi = new Kursi[jumlah];
+        this.initKursi();
+    }
+    private void initKursi() {
+    for (int i = 0; i < arrayKursi.length; i++) {
+        this.arrayKursi[i] = new Kursi(String.valueOf(i + 1));
+    }
+    }
+    public String info() {
+        String info = "";
+        info += "Kode: " + kode + "\n";
+        for (Kursi kursi : arrayKursi) {
+            info += kursi.info();
+        }
+        return info;
+    }
+    public void setPenumpang(Penumpang penumpang, int nomor) {
+        this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+    }
 }

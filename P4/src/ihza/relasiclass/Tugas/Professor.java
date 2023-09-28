2866e395a5b944751f5784cf4818f21768daefe3
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-28 21:08:06 +0700

    add Professor class

--- P4/src/ihza/relasiclass/Tugas/Professor.java
+++ P4/src/ihza/relasiclass/Tugas/Professor.java
@@ -1,4 +1,28 @@
 package ihza.relasiclass.Tugas;
 
-public class Professor {
+public class Professor extends Human {
+    private String employeeId;
+    private String subject;
+
+    public Professor(String name, int age, String address, String employeeId, String subject) {
+        super(name, age, address);
+        this.employeeId = employeeId;
+        this.subject = subject;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }

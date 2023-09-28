9ae72c4cb196357d618956267eded5878c6e8515
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-28 21:08:21 +0700

    add Student class

--- P4/src/ihza/relasiclass/Tugas/Student.java
+++ P4/src/ihza/relasiclass/Tugas/Student.java
@@ -1,2 +1,28 @@
-package ihza.relasiclass.Tugas;public class Student {
+package ihza.relasiclass.Tugas;
+
+public class Student extends Human {
+    private String studentId;
+    private String major;
+
+    public Student(String name, int age, String address, String studentId, String major) {
+        super(name, age, address);
+        this.studentId = studentId;
+        this.major = major;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
 }

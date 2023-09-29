b90b9d0815e02db919c95e35b261a2f38f640f2a
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 09:51:34 +0700

    add Course class

--- Kuis1/untitled/src/Kampus/Course.java
+++ Kuis1/untitled/src/Kampus/Course.java
@@ -1,2 +1,32 @@
-package Kampus;public class Course {
+package Kampus;
+
+class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course(String courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void displayInfo() {
+        System.out.println("Course ID: " + courseId);
+        System.out.println("Course Name: " + courseName);
+    }
 }

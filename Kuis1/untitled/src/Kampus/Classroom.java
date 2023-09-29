16dffa79157c282aad55cc7b2d4a42ab12d069d4
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 09:51:49 +0700

    add Classroom class

--- Kuis1/untitled/src/Kampus/Classroom.java
+++ Kuis1/untitled/src/Kampus/Classroom.java
@@ -1,2 +1,32 @@
-package Kampus;public class Classroom {
+package Kampus;
+
+class Classroom {
+    private String classroomId;
+    private String location;
+
+    public Classroom(String classroomId, String location) {
+        this.classroomId = classroomId;
+        this.location = location;
+    }
+
+    public String getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(String classroomId) {
+        this.classroomId = classroomId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void displayInfo() {
+        System.out.println("Classroom ID: " + classroomId);
+        System.out.println("Location: " + location);
+    }
 }

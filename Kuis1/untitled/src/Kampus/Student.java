6d554c26d58ec60a372caff14ae5a6c595e90f03
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 09:50:37 +0700

    add Student class

--- Kuis1/untitled/src/Kampus/Student.java
+++ Kuis1/untitled/src/Kampus/Student.java
@@ -1,2 +1,24 @@
-package Kampus;public class Student {
+package Kampus;
+
+class Student extends Person {
+    private String studentNIM;
+
+    public Student(String name, int age, String studentNIM) {
+        super(name, age);
+        this.studentNIM = studentNIM;
+    }
+
+    public String getStudentId() {
+        return studentNIM;
+    }
+
+    public void setStudentId(String studentNIM) {
+        this.studentNIM = studentNIM;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Student ID: " + studentNIM);
+    }
 }

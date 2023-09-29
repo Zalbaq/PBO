0c0072c3a6be6421875703abd8dbb735e1038d7f
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 09:50:55 +0700

    add Faculty class

--- Kuis1/untitled/src/Kampus/Faculty.java
+++ Kuis1/untitled/src/Kampus/Faculty.java
@@ -1,2 +1,24 @@
-package Kampus;public class Faculty {
+package Kampus;
+
+class Faculty extends Person {
+    private String employeeId;
+
+    public Faculty(String name, int age, String employeeId) {
+        super(name, age);
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Employee ID: " + employeeId);
+    }
 }

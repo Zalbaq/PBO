c3d049c63f3fec63653e603834ea44c12a4d7f20
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 09:51:14 +0700

    add Enrollment class

--- Kuis1/untitled/src/Kampus/Enrollment.java
+++ Kuis1/untitled/src/Kampus/Enrollment.java
@@ -1,2 +1,33 @@
-package Kampus;public class Enrollment {
+package Kampus;
+
+class Enrollment {
+    private Student student;
+    private Course course;
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Enrollment Information:");
+        student.displayInfo();
+        course.displayInfo();
+    }
 }
